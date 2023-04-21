353ae1aefe537dc816308833f4ae420a23075698
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-22 00:16:26 +0800

    fixed print statements

--- Lorena_Decryption.py
+++ Lorena_Decryption.py
@@ -1,5 +1,5 @@
 # First is to ask the user for their desired string to encrypt
-string = str(input("Enter your string here in LOWER CASE: "))
+string = str(input("Enter your string here: "))
 # Using the .replace method, we can easily replace a specific character from the user's string to any of our desired character
 decrypted = string.replace('*', 'a').replace('&', 'e').replace('#', 'i').replace('+', 'o').replace('!', 'u')
-print(decrypted)
+print("Your Decrypted Message:", decrypted)
